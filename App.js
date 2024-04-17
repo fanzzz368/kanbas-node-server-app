91723c22b5a5f6cd62026278a595dfbbe1b82cb6
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2024-04-17 17:01:54 -0400

    db for users

--- App.js
+++ App.js
@@ -4,12 +4,36 @@ import Lab5 from './Lab5.js';
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from './Kanbas/Users/routes.js';
+import session from "express-session";
+import "dotenv/config";
 
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas' || process.env.DB_CONNECTION_STRING 
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(cors(
+  {credentials: true,
+        origin: process.env.FRONTEND_URL}
+    ));
+const sessionOptions = {
+secret: process.env.SESSION_SECRET,
+resave: false,
+saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+};
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app);
 Lab5(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
